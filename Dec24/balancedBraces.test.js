9f901bdb5d7a4e7d952d223f90328c23f9711127
rahul-works <rahul.thakur9985@gmail.com>
2020-01-09 17:32:08 +0530

    remove Dec 24

--- Dec24/balancedBraces.test.js
+++
@@ -1,8 +0,0 @@
-const balancedBraces = require('./balancedBraces');
-
-describe('balancedBraces', () => {
-    it('Is balancedBraces', () => {
-        expect(balancedBraces('for(i=0;i<x[i];i++){if(x[i]<10){b++;}else{b+=10;}}')).toBe(true);
-        expect(balancedBraces('for(i=0;i<x[i];i++){if(x[i]<10){b++;}else{b+=10;}')).toBe(false);
-    });
-});
--- Dec24/pangram.test.js
+++
@@ -1,8 +0,0 @@
-const isPangram = require('./pangram');
-
-describe('pangram', () => {
-    it('checking pangram', () => {
-        expect(isPangram('the quick brown fox jumps over the lazy dog')).toBe(true);
-        expect(isPangram('a quick of the enemy will jeopardize gunboats')).toBe(false);
-    });
-});
