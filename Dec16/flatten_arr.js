9803e088f6269eca876cf294f856721de8701e68
rahul-works <rahul.thakur9985@gmail.com>
2020-01-09 12:05:41 +0530

    removing assignment solution from master

--- Dec16/flatten_arr.js
+++
@@ -1,7 +0,0 @@
-function flatten(arr) {
-  return arr.reduce(function (flat, toFlatten) {
-    return flat.concat(Array.isArray(toFlatten) ? flatten(toFlatten) : toFlatten);
-  }, []);
-}
-
-console.log(flatten([1, 2, [3, 4, [5, 6]]]));
