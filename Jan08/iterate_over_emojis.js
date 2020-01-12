f1185adef2ee23e402a3ef04fba2dc925df4d749
rahul-works <rahul.thakur9985@gmail.com>
2020-01-12 10:35:12 +0530

    iterate over emojis

---
+++ Jan08/iterate_over_emojis.js
@@ -0,0 +1,17 @@
+function emoji_print(){
+    let emojis = ['a', '😀', '😁', '😂', '🤣', '😃', '😄', '😅', 'b'];
+    for(const char of emojis){
+        console.log(char);
+    }
+}
+
+emoji_print();
+// a
+// 😀
+// 😁
+// 😂
+// 🤣
+// 😃
+// 😄
+// 😅
+// b
